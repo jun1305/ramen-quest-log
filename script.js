81fca17d85a96255956615a86478877f53c876ab
jun <jun.sbmk1305@gmail.com>
2025-06-24 12:39:58 +0900

    ok

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("It works 🚀");
+
+const cups = 3;
+console.log(`杯数:${cups}`);
+
+const logs = ["家系", "塩", "醤油"];
+const list = logs.map(style => `<li>${style}</li>`);
+console.log(list);
+
+import {add} from "./util.js";
+console.log(add(10,20));
